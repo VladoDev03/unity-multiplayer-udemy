6352e5f8cb598416d9bdb9a1f718f9dd849d7638
Vladimir Stoyanov <69723159+VladoDev03@users.noreply.github.com>
2025-11-11 00:56:10 +0200

    Added lobbies.

--- Unity Multiplayer/Assets/Scripts/Networking/Host/HostGameManager.cs
+++ Unity Multiplayer/Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using Unity.Networking.Transport.Relay;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
@@ -12,6 +16,7 @@ public class HostGameManager
 {
     private Allocation allocation;
     private string joinCode;
+    private string lobbyId;
 
     private const int MaxConnections = 20;
     private const string GameSceneName = "Game";
@@ -44,8 +49,50 @@ public class HostGameManager
 
         transport.SetRelayServerData(relayServerData);
 
-        NetworkManager.Singleton.StartHost();
+        try
+        {
+            CreateLobbyOptions lobbyOptions = new CreateLobbyOptions();
+            lobbyOptions.IsPrivate = false;
 
+            lobbyOptions.Data = new Dictionary<string, DataObject>()
+            {
+                {
+                    "JoinCode", new DataObject(
+                        visibility: DataObject.VisibilityOptions.Member,
+                        value: joinCode
+                    )
+                }
+            };
+
+            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(
+                "My Lobby",
+                MaxConnections,
+                lobbyOptions
+            );
+
+            lobbyId = lobby.Id;
+
+            HostSingleton.Instance.StartCoroutine(HeartBeatLobby(15));
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+            return;
+        }
+
+        NetworkManager.Singleton.StartHost();
         NetworkManager.Singleton.SceneManager.LoadScene(GameSceneName, LoadSceneMode.Single);
+    }
+
+    private IEnumerator HeartBeatLobby(float waitTimeSeconds)
+    {
+        WaitForSecondsRealtime delay = new WaitForSecondsRealtime(waitTimeSeconds);
+
+        while (true)
+        {
+            LobbyService.Instance.SendHeartbeatPingAsync(lobbyId);
+
+            yield return delay;
+        }
     }
 }
