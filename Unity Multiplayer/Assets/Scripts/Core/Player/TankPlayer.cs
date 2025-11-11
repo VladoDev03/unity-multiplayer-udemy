1c2405f99c603d5b5d324ae59c9339699a2a0afd
Vladimir Stoyanov <69723159+VladoDev03@users.noreply.github.com>
2025-11-12 00:29:10 +0200

    Added camera movement.

---
+++ Unity Multiplayer/Assets/Scripts/Core/Player/TankPlayer.cs
@@ -0,0 +1,20 @@
+using Unity.Cinemachine;
+using Unity.Netcode;
+using UnityEngine;
+
+public class TankPlayer : NetworkBehaviour
+{
+    [Header("References")]
+    [SerializeField] private CinemachineCamera virtualCamera;
+
+    [Header("Settings")]
+    [SerializeField] private int ownerPriority = 15;
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            virtualCamera.Priority = ownerPriority;
+        }
+    }
+}
